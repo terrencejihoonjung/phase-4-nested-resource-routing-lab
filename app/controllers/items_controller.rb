80980e71a300f6cca04876bd91afd05df2a8f06d
--replace-all <terrencejung928@gmail.com>
2022-07-21 18:55:42 -0700

    commit tree 6ff5173cd67f7c0d9989e26d9b1d1b70414275e6

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,29 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      items = User.find(params[:user_id]).items
+    else 
+      items = Item.all
+      return render json: items, include: :user
+    end
+    render json: items
   end
 
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    new_item = Item.create(name: params[:name], description: params[:description], price: params[:price], user_id: params[:user_id])
+    render json: new_item, status: :created
+  end
+
+  private 
+  
+  def not_found
+    render json: {error: "User not found"}, status: :not_found
+  end
 end
